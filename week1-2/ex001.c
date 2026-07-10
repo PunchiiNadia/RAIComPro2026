30eece855b6a651f04e63cac454e8e4820995adb
MsPunchitaT. <155294723+PunchiiNadia@users.noreply.github.com>
2026-07-10 07:54:12 +0000

    Commit changes

---
+++ week1-2/ex001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    printf("69011480 Punchita T.\n\n");
+    printf("%s\n", "Punchita T.");
+    printf("%d\n", 100);
+    printf("%f\n", 4.55);
+    printf("%c\n", 'A');
+    
+    printf("%s %d %f %c\n", "Punchita T.", 100, 4.55, 'A');
+    return 0;
+}
